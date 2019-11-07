da8b3c8cffaa577a90f98a09e33593a414223803
Daniela Perry <danielaperry2015@gmail.com>
2019-11-07 15:17:49 -0800

    added command line parameters
    for in and out files

--- scripts/replacements.sh
+++ scripts/replacements.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cat full_gene_list.txt | sed "s/C9ORF78/C9orf78/" | sed "s/LUC7LA/LUC7L3/" | sed "s/DGCR14/ESS2/" > full_gene_list_revised.txt
+# Usage: ./replacements.sh <GENELIST> <MODIFIED GENE LIST NAME>
+
+# Modify the necessary edits to the gene list below.
+# sed is a command for inline file editing, google it or type `man sed` in the terminal for more information
+# If you're new to this kind of thing the format is: sed "s/<old name>/<new name>/"
+
+cat $1 | sed "s/C9ORF78/C9orf78/" | sed "s/LUC7LA/LUC7L3/" | sed "s/DGCR14/ESS2/" > $2
