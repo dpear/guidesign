652d30955457bf737c006a9d0548f45ba48d034a
Daniela Perry <danielaperry2015@gmail.com>
2019-11-07 14:40:25 -0800

    Added genelist parameter and usage

--- scripts/lookups.sh
+++ scripts/lookups.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for line in $(cat full_gene_list.txt); do 
+# Usage: ./lookups.sh <GENELISt>
+# Returns all names in your gene list that do NOT appear in the gene table
+
+for line in $(cat $1); do 
 	c=$(grep --count $line ../chopchop/isoforms/hg38.gene_table)
 	if [ $c -eq 0 ]; then
 		echo $line
