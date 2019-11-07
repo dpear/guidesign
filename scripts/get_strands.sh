6ee62aff001af9934473ca015a07d21abb366a17
Daniela Perry <danielaperry2015@gmail.com>
2019-11-07 15:22:06 -0800

    add usage

--- scripts/get_strands.sh
+++ scripts/get_strands.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo 'gene	gene_strand' > ../files/strand_info.txt
+# Usage: ./get_strands.sh <GENELIST> <GENELIST WITH STRANDS OUTPUT FILE NAME>
 
-genes=$(cat full_gene_list_revised.txt)
+echo 'gene	gene_strand' > $2
+
+genes=$(cat $1)
 
 # extract the strand info from the gff file
 for gene in ${genes[@]}; do 
-	grep gene=$gene ../files/condensedGRCh38_latest_genomic.gff | sed "s/ /_/" | awk -v g="$gene" '{print g"\t"$7}' | head -n 1 >> ../files/strand_info.txt
+	grep gene=$gene ../files/condensedGRCh38_latest_genomic.gff | sed "s/ /_/" | awk -v g="$gene" '{print g"\t"$7}' | head -n 1 >> $2
 	count=$(grep -c gene=$gene ../files/condensedGRCh38_latest_genomic.gff)
 	if [ $count -eq 0 ]; then
 		echo $gene 'was not matched with strand'
