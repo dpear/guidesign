811f5763cee458d02463c0f9822fbb9a229dc577
Daniela Perry <danielaperry2015@gmail.com>
2019-11-08 11:03:34 -0800

    added parameters

--- scripts/get_strands.sh
+++ scripts/get_strands.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Usage: ./get_strands.sh <GENELIST> <GENELIST WITH STRANDS OUTPUT FILE NAME>
+# Usage: ./get_strands.sh <GENELIST> <GENELIST WITH STRANDS OUTPUT FILE NAME> <GFF FILE>
+# This assumes your gff file has strand information in the 7th column.
+inf=$1
+outf=$2
+gff=$3
 
-echo 'gene	gene_strand' > $2
-
-genes=$(cat $1)
+echo 'gene	gene_strand' > $outf
+genes=$(cat $inf)
 
 # extract the strand info from the gff file
 for gene in ${genes[@]}; do 
-	grep gene=$gene ../files/condensedGRCh38_latest_genomic.gff | sed "s/ /_/" | awk -v g="$gene" '{print g"\t"$7}' | head -n 1 >> $2
-	count=$(grep -c gene=$gene ../files/condensedGRCh38_latest_genomic.gff)
-	if [ $count -eq 0 ]; then
-		echo $gene 'was not matched with strand'
-	fi
+	grep gene=$gene $gff | sed "s/ /_/" | awk -v g="$gene" '{print g"\t"$7}' | head -n 1 >> $outf
+	count=$(grep -c gene=$gene $gff)
+	if [ $count -eq 0 ]; then echo $gene 'was not matched with strand'; fi
 done
 
 
